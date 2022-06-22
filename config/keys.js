7e44c67d3e1ba820bccd6170f0c6c392cc96f83e
Jay <jaredreyes039@gmail.com>
2022-06-21 18:41:09 -0700

    more secrets

--- config/keys.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    mongoURI : "mongodb+srv://admin:SuTcSnmciFgvWdi3@mern.ippdx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
-    netBearer: "jlbjVJLxl_EAh9H-DVntc-i6Ze1eY1MVI6AK5NBnxuE",
-    gitBearer: "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"
-}
