688820fcecfe6a308020645f549f9f742f704dd4
Jay <jaredreyes039@gmail.com>
2022-06-25 16:59:27 -0700

    Security Flaw Removal

--- config/keys.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    mongoURI : "mongodb+srv://admin:SuTcSnmciFgvWdi3@mern.ippdx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
-    netBearer: "jlbjVJLxl_EAh9H-DVntc-i6Ze1eY1MVI6AK5NBnxuE",
-    gitBearer: "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"
-}
