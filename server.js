c9d273fa52503603043a892894f9f77783744fe8
Jay <jaredreyes039@gmail.com>
2022-09-28 17:30:44 -0700

    Fixes?

--- server.js
+++ server.js
@@ -149,12 +149,7 @@ mongoose.connect(db)
     );
     
 
-    if(process.env.NODE_ENV === 'production'){
-        app.use(express.static(("client/build")))
-        app.get('*', (req, res)=>{
-            res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-        })
-    }
+
 
 // Server Init
 let userRouter = express.Router({ mergeParams : true })
